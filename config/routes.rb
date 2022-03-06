c67eabd63b724079003780dd2bec39eee64cce48
mokkiko <68779748+mokkiko@users.noreply.github.com>
2022-03-06 14:34:32 +0300

    create namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
-  resources :studios do
-    resources :movies
+
+  namespace :api do
+    namespace :v1 do
+      resources :studios do
+        resources :movies
+      end
+    end
+
+    namespace :v2 do
+      resources :studios
+      resources :movies
+    end
   end
-  root "studios#index"
+
+  
+    root "studios#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
